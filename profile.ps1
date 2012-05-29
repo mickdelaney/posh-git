99659845cbb60139911ad881477af9651210fc74
mickdelaney <mickdelaney@gmail.com>
2012-05-29 11:04:53 +0100

    Added Module For Visual Studio Commands

--- profile.ps1
+++ profile.ps1
@@ -23,22 +23,14 @@ function prompt {
     return "> "
 }
 
+Import-Module .\VisualStudioCommandPrompt
+Enable-VisualStudioCommandPrompt
+
 Enable-GitColors
 
 Pop-Location
 
 Start-SshAgent -Quiet
 
-set-location D:\elevate
-
-
-#Set environment variables for Visual Studio Command Prompt
-pushd 'c:\Program Files (x86)\Microsoft Visual Studio 10.0\VC'
-cmd /c "vcvarsall.bat&set" |
-foreach {
-  if ($_ -match "=") {
-    $v = $_.split("="); set-item -force -path "ENV:\$($v[0])"  -value "$($v[1])"
-  }
-}
-popd
-write-host "`nVisual Studio 2010 Command Prompt variables set." -ForegroundColor Yellow
+Import-Module D:\elevate\elevate-app\elevate-profile-extensions.psm1
+Set-location D:\elevate
